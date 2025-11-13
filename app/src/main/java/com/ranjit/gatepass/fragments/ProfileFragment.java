cfabc1bd07cb3f85572d27bc1424b23d5fd7a00e
Ranjit <patilranjit485@gmail.com>
2025-11-13 17:20:20 +0530

    User can logout..

--- app/src/main/java/com/ranjit/gatepass/fragments/ProfileFragment.java
+++ app/src/main/java/com/ranjit/gatepass/fragments/ProfileFragment.java
@@ -2,6 +2,9 @@ package com.ranjit.gatepass.fragments;
 
 import static android.content.Context.MODE_PRIVATE;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 
@@ -13,13 +16,15 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.imageview.ShapeableImageView;
 import com.ranjit.gatepass.R;
+import com.ranjit.gatepass.activity.LoginActivity;
 
 public class ProfileFragment extends Fragment {
 
     TextView tvName, tvEmail, tvRole, tvDepartment, tvGender, tvParentMobile,tvUserMobile;
-
+    SharedPreferences prefs;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -32,8 +37,9 @@ public class ProfileFragment extends Fragment {
         tvGender = view.findViewById(R.id.tvGender);
         tvUserMobile = view.findViewById(R.id.tvUserMobile);
         tvParentMobile = view.findViewById(R.id.tvParentMobile);
+        MaterialToolbar toolbar = view.findViewById(R.id.toolbar);
 
-        SharedPreferences prefs = getActivity().getSharedPreferences("GatePassApp", MODE_PRIVATE);
+        prefs = getActivity().getSharedPreferences("GatePassApp", MODE_PRIVATE);
         String name = prefs.getString("name", "User");
         String email = prefs.getString("email", "-");
         String role = prefs.getString("role", "-");
@@ -50,6 +56,25 @@ public class ProfileFragment extends Fragment {
         tvGender.setText(gender);
         tvUserMobile.setText(userMobile);
         tvParentMobile.setText(userMobile);
+
+        toolbar.setNavigationOnClickListener(v -> {
+            new AlertDialog.Builder(getContext())
+                    .setTitle("Logout")
+                    .setMessage("Are you sure you want to logout?")
+                    .setPositiveButton("Yes", (dialog, which) -> {
+                        // Handle logout here
+                        SharedPreferences.Editor editor = prefs.edit();
+                        editor.clear();
+                        editor.apply();
+
+                        Intent intent = new Intent(getContext(), LoginActivity.class);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        getContext().startActivity(intent);
+                    })
+                    .setNegativeButton("No", (dialog, which) -> dialog.dismiss())
+                    .show();
+        });
+
 
         ShapeableImageView profileImage = view.findViewById(R.id.profile_image); // set proper ID
         String imageUrl = prefs.getString("profileImage", "-");
